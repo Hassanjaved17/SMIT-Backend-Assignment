d572dce771cedd62d1676cfb30e0282a342ade2b
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-21 00:52:11 +0500

    style: add comments to clarify routes in userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,8 +3,8 @@ import { signup, login, getUsers } from "../controller/userController.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.get("/", getUsers);
+router.post("/signup", signup); // Route for user signup
+router.post("/login", login); // Route for user login
+router.get("/", getUsers); // Route for getting all users
 
 export default router;

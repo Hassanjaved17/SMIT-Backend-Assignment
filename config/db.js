413a3b80673156c7c6290e663cf4b02c2bbc9ab5
Hassanjaved17 <hassanaptech9@gmail.com>
2026-07-20 23:10:25 +0500

    docs: add comments to db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    // Connect to MongoDB
     const conn = await mongoose.connect(process.env.MONGO_URI);
+    // Log successful connection
+
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
+    // Log error if connection fails
+
     console.log(`MongoDB connection error: ${error.message}`);
     process.exit(1); // stop the server if DB connection fails
   }
